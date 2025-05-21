04afe08b14f7830258de6e9894b3e927617ba73b
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-05-22 02:11:41 +0900

    feat: add websocket process

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    // Create object
+    const socket = new WebSocket("ws://127.0.0.1:5000");
+    // Connect server
+    socket.addEventListener("open", (_) => {
+      socket.send("Hello from client!");
+    });
+    // Listen for message
+    socket.addEventListener("message", (event) => {
+      console.log(event.data);
+      setMessage(event.data);
+    });
+  }, []);
   return (
     <>
       <h1>axum-websocket-mytutorial</h1>
+      <p>Recieved Message: {message}</p>
     </>
   );
 }
